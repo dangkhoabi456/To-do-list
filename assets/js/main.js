3da15f1d8dbfea4b1921a48229405f2dcc8a4699
dangkjoabi80Âgmail.com <dangkhoabi80@gmail.com>
2025-08-16 21:00:57 +0700

    Add the foundation of the web page the add-task section

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.section');
+const itemList = document.querySelectorAll('.item-list');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      // Bỏ active ở tất cả link
+      navLinks.forEach(item => item.classList.remove('active'));
+      // Thêm active vào link được click
+      this.classList.add('active');
+
+      itemList.forEach(section => section.style.display = 'none');
+      const targetId = this.getAttribute('data-target');
+      document.getElementById(targetId).style.display = 'block';
+
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('add-task').addEventListener('click', function(event){
+        event.preventDefault();
+        const taskName = document.getElementById("task-name").value;
+        const taskDescription = document.getElementById("task-description").value;
+        const taskDeadline = document.getElementById("task-deadline").value;
+        
+        console.log("Task Deadline: " + taskDeadline);
+        console.log("Task Name: " + taskName);
+        console.log("Task Description: " + taskDescription);
+    });
+});
+
+document.getElementById('add-task').addEventListener('click', function(){
+    console.log("Button clicked!");
+});
+
+let number = 1;
+console.log("Number: " + number);
